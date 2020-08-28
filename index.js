71ac507113696208cd47c806fc963905f3e6a707
Gian Carlo Mantuan <gian.ti@grupointegrado.br>
2020-08-27 23:27:04 -0300

    add regex for youtube links

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const dotenv = require("dotenv");
 const wordDictionary = require("./dictionaries/Words_Dictionary.json");
 const replyDictionary = require("./dictionaries/Reply_Dictionary.json");
 const brabaDictionary = require("./dictionaries/Braba_Dictionary.json");
-const badDictionary = require("./dictionaries/Bad_Words.json");
 
 // Bot Configuration:
 const Bot = new Discord.Client();
@@ -21,20 +20,13 @@ Bot.once("ready", () => {
 
 // Bot Checks:
 Bot.on("message", (msg) => {
-
-   //Return true if word is literal or have any sub-string in himself
-   let n_word = content.split(" ").some((w) => {
-    return (
-          // Strip the string and compare if the word is equal other 
-            badDictionary.literal.find((k) => k === w) ||
-          // Check if have word inside the string 
-            badDictionary.like.find((k) => k.includes(k)));
-  });
-
-  if (msg.author.bot && !n_word) return;
-
   const content = msg.content.toLowerCase();
+  const ytUri = /(?:http(?:s)?:\/\/)?(?:www\.)?(?:youtu\.be\/|youtube\.com\/(?:(?:watch)?\?(?:.*&)?v(?:i)?=|(?:embed|v|vi|user)\/))([^\?&\"'<> #]+)/gim;
 
+  // Check if the author of the message is the bot itself
+  if (msg.author.bot) return;
+
+  // Switch Message Content:
   switch (content) {
     case "lansa a braba bolsonaro":
       msg.channel.send(
@@ -45,18 +37,25 @@ Bot.on("message", (msg) => {
       msg.channel.send(
         "\n [lansa a braba bolsonaro]: Pede uma braba para o bolsonaro \n [!help]: Opções de ajuda para o bot \n Qualquer outro texto no dicionário o bot retornará uma mensagem \n"
       );
-    default:
-      for (const dict in wordDictionary) {
-        if (wordDictionary.hasOwnProperty(dict)) {
-          word = wordDictionary[`${dict}`];
-
-          word.some((check) => {
-            reply = replyDictionary[`${dict}`];
-            if (content.includes(check))
-              msg.reply(reply[Math.floor(Math.random() * reply.length)]);
-          });
-        }
-      }
       break;
-  }
+    default:
+      // Split the message:
+      content.split(" ").some((sContent) => {
+        // Check if youtube link
+        if (sContent.match(ytUri)) return;
+
+        for (const dict in wordDictionary) {
+          if (wordDictionary.hasOwnProperty(dict)) {
+            word = wordDictionary[`${dict}`];
+
+            word.some((check) => {
+              reply = replyDictionary[`${dict}`];
+              if (sContent.includes(check))
+                msg.reply(reply[Math.floor(Math.random() * reply.length)]);
+            });
+          }
+        }
+      }); // End Some Function
+      break;
+  } // End Switch Message Content
 }); // End Bot.on
